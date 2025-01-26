2c93764597ea81b76ed79bafa3540b8ca6e381b2
Julia <lorrayne@usf.edu>
2025-01-26 12:09:26 -0500

    Task 2 - Product Details.

--- cc_1.js
+++ cc_1.js
@@ -4,3 +4,10 @@ const employeeID = 1234;
 console.log(typeof employeeID);
 var isActive = true;
 console.log(typeof isActive);
+
+let productName = 'pen';
+const productPrice = 1.50;
+var isAvailable = true;
+console.log(typeof productName);
+console.log(typeof employeeID);
+console.log(typeof isAvailable);
